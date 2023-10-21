9a59ae7fa1f94d5ea23202813224a32e2111964c
Sudeepa Pal <85659225+sudeepa88@users.noreply.github.com>
2023-10-21 09:35:30 +0530

    Initial Commit

---
+++ Circular_view_progresss/Circular_view_progresssApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circular_view_progresssApp.swift
+//  Circular_view_progresss
+//
+//  Created by sudeepa pal on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Circular_view_progresssApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Circular_view_progresss/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Circular_view_progresss
+//
+//  Created by sudeepa pal on 21/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
